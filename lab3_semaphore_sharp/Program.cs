793ff505178a2aa2d002fb3762314e9adfeec8b1
Max Ponomarenko <maxeleron@gmail.com>
2024-05-14 21:52:42 +0300

    Lab3. Initial commit

---
+++ lab3_semaphore_sharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab3_semaphore_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
